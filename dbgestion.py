a6b43b9eb0f8b8e94412e16c3c5250b78e1768b7
Brushiti <blerta.rushiti@ulb.be>
2025-05-18 11:13:36 +0200

    Update dbgestion.py

--- dbgestion.py
+++ dbgestion.py
@@ -13,6 +13,7 @@ class DB:
                         VALUES (?,?,?,?,?,?) RETURNING idperson;""", (Name, LastName, DateOfBirth, Email,Password, Role))   #returning the Idperson because it is generated automatically
         #Retrieve the added person ID
         personid = cur.fetchone()[0]
+        self.con.commit()
         cur.close()
         return personid
     
@@ -27,6 +28,7 @@ class DB:
         iddoctor = cur.fetchone()[0]
         #We update the person associated to the doctor in order to be able to find doctor from person
         cur.execute("""UPDATE person SET iddoctor = ? WHERE idperson = ?;""", (iddoctor, idperson))     #permet de relier ensemble les deux lignes et ainsi savoir que ce sont les mêmes
+        self.con.commit()
         cur.close()
         return iddoctor
     
@@ -105,20 +107,13 @@ class DB:
         rows = cur.fetchall()
         cur.close()
         return  [dict(zip(['temperature', 'tension', 'datetime'], row)) for row in rows]
+
+
+    def __del__(self):
+        #In the case where the object would destroyed, we want our connection to end smmothly, with commit and close.
+        self.con.commit()
+        self.con.close()
     
-    def get_id_doctor(self, email):
-        cur = self.con.cursor()
-        cur.execute("""SELECT iddoctor FROM person WHERE email = ?;""", (email,))
-        iddoctor = cur.fetchone()
-        return iddoctor[0]
-
-    def get_id_patient(self, email):
-        cur = self.con.cursor()
-        cur.execute("""SELECT idpatient FROM person WHERE email = ?;""", (email,))
-        idpatient = cur.fetchone()
-        cur.close()
-        return idpatient[0]
-
     def show_table(self, table_name):
         cur = self.con.cursor()
         cur.execute(f"SELECT * FROM {table_name};")
@@ -127,7 +122,56 @@ class DB:
         for row in rows:
             print(row)
 
+    def add_prescription(self, idpatient, medicament, frequence):
+        cursor = self.con.cursor()
+        print("add_prescription called")
+        cursor.execute("""
+            INSERT INTO prescription (idpatient, medicament, frequence, prise, horodatage)
+            VALUES (?, ?, ?, 0, datetime('now'))
+        """, (idpatient, medicament, frequence))
+        self.con.commit()
 
+    def get_prescriptions(self, idpatient):
+        cursor = self.con.cursor()
+        cursor.execute("""
+            SELECT idprescription, medicament, frequence, prise
+            FROM prescription
+            WHERE idpatient = ?
+        """, (idpatient,))
+        return cursor.fetchall()
+
+    def mark_as_taken(self, idprescription):
+        cursor = self.con.cursor()
+        cursor.execute("""
+            UPDATE prescription
+            SET prise = 1
+            WHERE idprescription = ?
+        """, (idprescription,))
+        self.con.commit()
+
+    def get_patient_prescriptions(self, idpatient):
+        cursor = self.con.cursor()
+        cursor.execute("""
+            SELECT medicament, frequence, prise
+            FROM prescription
+            WHERE idpatient = ?
+        """, (idpatient,))
+        rows = cursor.fetchall()
+        return [{'medicament': r[0], 'frequence': r[1], 'prise': r[2]} for r in rows]
+    
+    def get_id_doctor(self, email):
+        cur = self.con.cursor()
+        cur.execute("""SELECT iddoctor FROM person WHERE email = ?;""", (email,))
+        iddoctor = cur.fetchone()
+        return iddoctor[0]   
+
+    def get_id_patient(self, email):
+        cur = self.con.cursor()
+        cur.execute("""SELECT idpatient FROM person WHERE email = ?;""", (email,))
+        idpatient = cur.fetchone()
+        cur.close()
+        return idpatient[0]
+    
     def authentification(self, email, password):
         cur = self.con.cursor()
         cur.execute("""SELECT role, password FROM person WHERE email = ?;""", (email,))
@@ -137,20 +181,69 @@ class DB:
         elif res[0][1] == password:
             return res[0][0]               #return = role
         else: 
-            return 1                    #there is no correspondance between the password and the email 
+            return 1                    #there is no correspondance between the password and the email
 
-    def __del__(self):
-        #In the case where the object would destroyed, we want our connection to end smmothly, with commit and close.
-        self.con.commit()
-        self.con.close()
+
+if __name__ == "__main__":
+    # Création d'une instance de la base
+    db = DB("projet.db")
+
+    # 1. Ajouter un docteur
+    iddoctor1 = db.insert_doctor(
+        Name="John", LastName="Doe", DateOfBirth="1975-06-15",
+        Email="j.doe@hospital.com", Password="secure123"
+    )
+    iddoctor2 = db.insert_doctor(
+        Name='Louis',LastName= "Chirong", DateOfBirth="2013", 
+        Email="Loulouchichi@hotmail.com", Password="loulouchichi"
+    )
+
+    # 2. Ajouter deux patients
+    idpatient1 = db.insert_patient(
+        PhoneNumber="0456112233", DateOfBirth="1990-01-01",
+        Name="Alice", LastName="Durand", Email="alice@example.com",
+        Password="alicepwd"
+    )
+
+    idpatient2 = db.insert_patient(
+        PhoneNumber="0456445566", DateOfBirth="1988-04-21",
+        Name="Bob", LastName="Martin", Email="bob@example.com",
+        Password="bobpwd"
+    )
+
+    idpatient3 = db.insert_patient(PhoneNumber="0478521362", DateOfBirth="2001-04-07",
+                                   Name='Juliette', LastName='Dedong', Email= "jdedong@gmail.com",
+                                   Password="123456")
     
-    
-    
+    idpatient4 = db.insert_patient(PhoneNumber="0478521362", DateOfBirth="2001-04-07",
+                                   Name='Un', LastName='Deux', Email= "undeux@gmail.com",
+                                   Password="123456")
 
+    # 3. Lier le docteur aux deux patients
+    db.link_doctor_to_patient(iddoctor1, idpatient1)
+    db.link_doctor_to_patient(iddoctor1, idpatient2)
+    db.link_doctor_to_patient(iddoctor2, idpatient3)
+    db.link_doctor_to_patient(iddoctor2, idpatient4)
 
+    # 4. Ajouter des données médicales pour un patient
+    db.insert_patient_data(idpatient1, temperature=37.8, tension="125/85")
+    db.insert_patient_data(idpatient1, temperature=38.2, tension="130/90")
+    db.insert_patient_data(idpatient3, 37.8, '125/80')
+    db.insert_patient_data(idpatient4, 37.5, '110/70')
+    db.insert_patient_data(idpatient4, 37.5, '125/80')
+    db.insert_patient_data(idpatient4, 40, '135/85')
 
+    print("Docteur, patients et données ajoutés avec succès.")
 
+    # 5. Print toutes les tables ainsi voir si ça fonctionne bien
 
-   
-    
+    db.show_table('person')
+    print('----------------------')
+    db.show_table('patient')
+    print('----------------------')
+    db.show_table('doctor')
+    print('----------------------')
+    db.show_table('patient_data')
+    print('----------------------')
+    db.show_table('doctor_patient')
 
